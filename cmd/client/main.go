686b43a0adced15c651caeaad0b47e5fc720eb6b
liyouxin <liyouxin@youzan.com>
2023-08-10 12:42:31 +0800

    more

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
-	"os"
+	"strconv"
 )
 
 func sender(conn net.Conn) {
@@ -17,18 +17,22 @@ func sender(conn net.Conn) {
 }
 
 func main() {
-	server := "127.0.0.1:20000"
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", server)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
-		os.Exit(1)
+	//server := "127.0.0.1:20000"
+	//tcpAddr, err := net.ResolveTCPAddr("tcp4", server)
+	//if err != nil {
+	//	fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+	//	os.Exit(1)
+	//}
+	//conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	//if err != nil {
+	//	fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+	//	os.Exit(1)
+	//}
+	//
+	//fmt.Println("connect success")
+	//sender(conn)
+	i := 0
+	for i = 0; i <= 11; i++ {
+		fmt.Println("curl http://www.cnga.org.cn/cngaresource/brand.php?totalresult=1135&pageno=" + strconv.Itoa(i) + " >> " + strconv.Itoa(i) + ".html")
 	}
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
-		os.Exit(1)
-	}
-
-	fmt.Println("connect success")
-	sender(conn)
 }
